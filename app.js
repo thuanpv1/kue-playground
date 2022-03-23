d3f5a267f94fe785961e2e5c156c3f6fc4039dfe
thuanpv2011 <thuanpv2011@gmail.com>
2022-03-23 23:07:38 +0700

    fix bugs

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-import { Response, Request } from "express";
-import { Job, DoneCallback } from "kue";
-import axios from "axios";
+const axios = require("axios");
 
 const kue = require("kue");
 const express = require("express");
@@ -8,7 +6,7 @@ const app = express();
 const port = 3000;
 const queue = kue.createQueue();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   for (let i = 1; i <= 20; i++) {
     queue
       .create("queue example", {
@@ -16,20 +14,26 @@ app.get("/", (req: Request, res: Response) => {
         data: i
       })
       .priority("high")
+      .attempts(5)
       .save();
   }
   res.send("Hello World!");
 });
-queue.process("queue example", (job: Job, done: DoneCallback) => {
+queue.process("queue example", (job, done) => {
   axios
     .get("https://jsonplaceholder.typicode.com/todos/" + job.data.data)
     .then(result => {
-      console.log(result.data);
+      console.log('result.data====', result.data);
       done();
       return result.data;
     })
-    .catch(error => done(error));
+    .catch(error => {
+      console.log('error===', error)
+      
+      done(error)
+      });
 });
+
 app.use("/kue-api/", kue.app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
